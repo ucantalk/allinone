47a170c10e214a68a2226d5a084a737a6230f4db
soso <soso@eric>
2016-07-05 10:41:02 +0800

    2016/7/5

--- src/main/java/com/aio/exception/NullUserScoreException.java
+++ src/main/java/com/aio/exception/NullUserScoreException.java
@@ -1,8 +1,5 @@
 package com.aio.exception;
 
-import com.sun.tools.internal.ws.wsdl.document.jaxws.CustomName;
-import com.sun.tools.internal.ws.wsdl.document.jaxws.Exception;
-
 public class NullUserScoreException extends Throwable {
 	public NullUserScoreException() {
 	};
